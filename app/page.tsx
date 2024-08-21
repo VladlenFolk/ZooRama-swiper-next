f1e1c77c97efa860b92b263ba588f49e6d14b8e9
Avok@to34 <vladlen-17@yandex.ru>
2024-08-21 22:30:38 +0300

    feat: перенес содержимое из проекта на реакт

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,123 @@
-import Image from "next/image";
+'use client'
+import { useState, useRef, createRef, useMemo } from "react";
+import "./App.css";
+import Button from "@/app/components/button/button";
 
-export default function Home() {
+interface Animal {
+  id: number;
+  name: string;
+  img: string;
+}
+
+type Direction = "left" | "right" | "up" | "down";
+
+interface CardRef {
+  swipe: (dir?: Direction) => Promise<void>;
+  restoreCard: () => Promise<void>;
+}
+
+const animals: Animal[] = [
+  {
+    id: 1,
+    name: "Доша",
+    img: "https://avatars.mds.yandex.net/get-entity_search/2360676/844472719/S122x122_2x",
+  },
+  {
+    id: 2,
+    name: "Сиба",
+    img: "https://avatars.mds.yandex.net/get-entity_search/135316/777333296/S122x122Smart_2x",
+  },
+  {
+    id: 3,
+    name: "Шпиц",
+    img: "https://avatars.mds.yandex.net/get-entity_search/1922058/849472009/S122x122Smart_2x",
+  },
+  {
+    id: 4,
+    name: "Джек",
+    img: "https://avatars.mds.yandex.net/get-entity_search/7689070/784457321/S122x122Smart_2x",
+  },
+];
+
+function App() {
+  const [currentIndex, setCurrentIndex] = useState<number>(animals.length - 1);
+  const [lastDirection, setLastDirection] = useState<Direction | undefined>();
+
+  const currentIndexRef = useRef<number>(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(animals.length)
+        .fill(0)
+        .map(() => createRef<CardRef>()),
+    []
+  );
+
+  const updateCurrentIndex = (val: number) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < animals.length - 1;
+  const canSwipe = currentIndex >= 0;
+
+  const swiped = (
+    direction: Direction,
+    index: number
+  ) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const swipe = async (dir?: Direction) => {
+    if (canSwipe && currentIndex < animals.length) {
+      await childRefs[currentIndex].current?.swipe(dir);
+    }
+  };
+
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current?.restoreCard();
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="app">
+      <div className="flex flex-col items-center content-center w-screen h-screen min-h-600 p-5 relative">
+        <div className="flex flex-col h-300 max-w-260 relative w-90v">
+          {animals.map((animal) => (
+            <div
+              // ref={childRefs[index]}
+              className="origin-center w-full h-full absolute"
+              key={animal.id}
+              // onSwipe={(dir) => swiped(dir as Direction, animal.name, index)}
+            >
+              <div
+                style={{ backgroundImage: `url(${animal.img})` }}
+                className="absolute rounded-[20px] bg-white max-w-260 h-[260px] bg-cover bg-center w-80v shadow-card"
+              >
+                <h3 className="absolute bottom-4 right-4 text-white font-bold text-lg ">{animal.name}</h3>
+              </div>
+            </div>
+          ))}
         </div>
+        <div className="flex flex-col sm:flex-row sm:flex-wrap justify-center">
+          <Button text={'Смахнуть влево!'} swipe={() => swipe("left")} canSwipe={canSwipe} />
+           <Button text={'Предыдущий!'} swipe={() => goBack()} canSwipe={canGoBack} />
+           <Button text={'Смахнуть вправо!'} swipe={() => swipe("right")} canSwipe={canSwipe} />
+        </div>
+        {lastDirection ? (
+          <h2 key={lastDirection} className="infoText">
+            You swiped {lastDirection}
+          </h2>
+        ) : (
+          <h2 className="infoText">
+            Swipe a card or press a button to get Restore Card button visible!
+          </h2>
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+export default App;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      minHeight:{
+        '600': '600px'
+      },
+      height:{
+        '300': '300px'
+      },
+      maxWidth:{
+        '260': '260px'
+      },
+      width:{
+        '90v': '90vw',
+        '80v': '80vw',
+      },
+      boxShadow:{
+        'shadow-card': '0px 0px 60px 0px rgba(0,0,0,0.30)',
+        'shadow-button': '0px 0px 30px 0px rgba(0,0,0,0.10)',
+      },
+      colors: {
+        'button-color': '#9198e5',
+      },
     },
   },
   plugins: [],
