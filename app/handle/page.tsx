ba0e79b99741ecc0288cca7eb20f10b630d695ff
Avok@to34 <vladlen-17@yandex.ru>
2024-10-01 23:31:09 +0300

    feat: сделал карточку

---
+++ app/handle/page.tsx
@@ -0,0 +1,10 @@
+
+export default function HandleSwiper() {
+
+    
+    return (
+        <div className="flex flex-col  items-center h-screen justify-center">
+            <div className="w-[100px] h-[200px] bg-gray-700" ></div>
+        </div>
+    )
+}
