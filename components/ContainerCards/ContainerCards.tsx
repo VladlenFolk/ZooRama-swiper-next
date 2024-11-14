f7e641a0bf8e4f78ea47e525fd88bcbe6939af41
Avok@to34 <vladlen-17@yandex.ru>
2024-11-14 23:54:06 +0300

    feat: added data and item in cards

--- components/ContainerCards/ContainerCards.tsx
+++ components/ContainerCards/ContainerCards.tsx
@@ -1,14 +1,22 @@
-import { Status } from "@/utils/interfaces";
+import { Data, Status } from "@/utils/interfaces";
+import { CardItem } from "../CardItem/CardItem";
+import { data } from "@/utils/data";
 
 interface Props {
+  items: Data[];
   status: Status;
 }
 
-export const ContainerCards = ({ status }: Props) => {
+export const ContainerCards = ({ items = [], status }: Props) => {
   return (
     <div className="border-2 border-solid border-[#ffffff8c] rounded-[5px] transition-all delay-300 ease-linear ">
-      <p className="rounded-[5px] bg-black text-center py-4 font-bold capitalize">{status}</p>
-      {/* <Cards></Cards> */}
+      <p className="rounded-[5px] bg-black text-center py-4 font-bold capitalize">
+        {status} hero
+      </p>
+      {items.map(
+        (item) =>
+          status === item.status && <CardItem data={item} key={item.id} />
+      )}
     </div>
   );
 };
--- components/DragAndDrop/DragAndDrop.tsx
+++ components/DragAndDrop/DragAndDrop.tsx
@@ -1,5 +1,6 @@
 import { Status } from "@/utils/interfaces";
 import { ContainerCards } from "../ContainerCards/ContainerCards";
+import { data } from "@/utils/data";
 
 const typesHero: Status[] = ["good", "normal", "bad"];
 
@@ -7,7 +8,7 @@ export const DragAndDrop = () => {
   return (
     <div className="grid grid-cols-3 gap-8 px-8 w-full flex-1">
       {typesHero.map((container) => (
-        <ContainerCards status={container} key={container} />
+        <ContainerCards status={container} key={container} items={data} />
       ))}
     </div>
   );
