889547120ea35c6e9876d3d080e0b0abdaa52cf5
Avok@to34 <vladlen-17@yandex.ru>
2024-11-17 18:21:00 +0300

    feat: create hook for DnD

--- components/DragAndDrop/DragAndDrop.tsx
+++ components/DragAndDrop/DragAndDrop.tsx
@@ -1,26 +1,14 @@
-"use client";
+'use client'
 import { Status } from "@/utils/interfaces";
 import { ContainerCards } from "../ContainerCards/ContainerCards";
 import { data } from "@/utils/data";
-import { useState } from "react";
-import { Data } from "@/utils/interfaces";
+import { useDragAndDrop } from "@/hooks/useDragAndDrop";
 
 const typesHero: Status[] = ["good", "normal", "bad"];
 
 export const DragAndDrop = () => {
-  const [isDragging, setIsDragging] = useState(false);
-  const [listItems, setListItems]= useState<Data[]>(data);
-  const handleUpdateList  = (id: number, status: Status) =>{
-    let card = listItems.find(item =>item.id === id)
-    if(card && card.status !== status) {
-      card.status = status
-      setListItems(prev=> ([
-        card!, 
-        ...prev.filter(item=>item.id !== id)
-      ]))
-    }
-  }
-  const handleDragging = (dragging: boolean) => setIsDragging(dragging);
+  const { isDragging, listItems, handleDragging, handleUpdateList } =
+    useDragAndDrop(data);
   return (
     <div className="grid grid-cols-3 gap-8 px-8 w-full flex-1">
       {typesHero.map((container) => (
@@ -28,7 +16,6 @@ export const DragAndDrop = () => {
           status={container}
           key={container}
           items={listItems}
-
           isDragging={isDragging}
           handleDragging={handleDragging}
           handleUpdateList={handleUpdateList}
