7ef80ce2ebee2e524e1237e91540bad66695aec6
Avok@to34 <vladlen-17@yandex.ru>
2024-11-25 23:49:02 +0300

    feat: brought out information about dogs into  data.ts

--- utils/data.ts
+++ utils/data.ts
@@ -1,24 +1,48 @@
 import { Data } from "./interfaces";
 
 export const data: Data[] = [
-    {
-        id: 1,
-        content: 'Aqua-man',
-        status: 'good'
-    },
-    {
-        id: 2,
-        content: 'Flash',
-        status: 'normal'
-    },
-    {
-        id: 3,
-        content: 'Green Lantern',
-        status: 'good'
-    },
-    {
-        id: 4,
-        content: 'Batman',
-        status: 'bad'
-    }
-]
+  {
+    id: 1,
+    content: "Aqua-man",
+    status: "good",
+  },
+  {
+    id: 2,
+    content: "Flash",
+    status: "normal",
+  },
+  {
+    id: 3,
+    content: "Green Lantern",
+    status: "good",
+  },
+  {
+    id: 4,
+    content: "Batman",
+    status: "bad",
+  },
+];
+
+type Card = {
+  id: number;
+  img: string;
+};
+
+export const card: Card[] = [
+  {
+    id: 1,
+    img: "https://avatars.mds.yandex.net/get-altay/1880508/2a0000016e256861de17b2bc4fbf2ce81b1a/XXL",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/736x/61/b7/72/61b772864029519c45dcbdf78680cdbf.jpg",
+  },
+  {
+    id: 3,
+    img: "https://avatars.mds.yandex.net/get-entity_search/5513561/952894097/S600xU_2x",
+  },
+  {
+    id: 4,
+    img: "https://avatars.mds.yandex.net/get-entity_search/2069560/952076372/S600xU_2x",
+  },
+];
