29ca70a240d1ce1cc2dc486bbaec45ec52868e1a
Avok@to34 <vladlen-17@yandex.ru>
2024-12-01 12:08:03 +0300

    feat: made the inscriptions appear smoothly

--- components/DraggableCard/DraggableCard.tsx
+++ components/DraggableCard/DraggableCard.tsx
@@ -21,15 +21,11 @@ const DraggableCard: React.FC<Props> = ({
   length,
   isResetting,
 }) => {
- 
-
-  const { elementRef, handleDown, resetAllState, translateX, windowWidth } = useDrag(
-    () => {
+  const { elementRef, handleDown, resetAllState, translateX, windowWidth } =
+    useDrag(() => {
       // Проверяем, что функция вызывается только для активной карточки
       if (length - index === counter) handleIncrease();
-    },
-    isResetting
-  );
+    }, isResetting);
   const isActive = length - index === counter;
 
   useEffect(() => {
@@ -37,8 +33,14 @@ const DraggableCard: React.FC<Props> = ({
       resetAllState();
     }
   }, [isResetting]);
-console.log(translateX);
+  console.log(translateX);
 
+  // Максимальное значение при котором opacity становится 1
+  const maxX = windowWidth / 10 + 10;
+
+  // Нормализация значения opacity от 0 до 1 для обоих направлений
+  const likeOpacity = Math.min(Math.max((translateX - 0) / maxX, 0), 1);
+  const nopeOpacity = Math.min(Math.max((0 - translateX) / maxX, 0), 1);
 
   return (
     <>
@@ -57,18 +59,20 @@ console.log(translateX);
             }`}
           >
             <div
-              className={`${`absolute  z-10 text-center w-[70px] text-[1rem] font-bold  text-white pointer-events-none  transition-opacity duration-200 ease-in-out`}
-                      ${"top-[20px] bg-[green] left-[20px] p-[10px] rounded-[5px] rotate-[-30deg]"} ${
-                translateX > windowWidth / 10 + 10 ? "opacity-100" : "opacity-0"
-              }`}
+              className={`${`absolute  z-10 text-center w-[70px] text-[1rem] font-bold  text-white pointer-events-none  `}
+                      ${"top-[20px] bg-[green] left-[20px] p-[10px] rounded-[5px] rotate-[-30deg]"} `}
+              style={{
+                opacity: likeOpacity,
+              }}
             >
               LIKE
             </div>
             <div
-              className={`${"absolute z-10  text-center w-[70px] text-[1rem] font-bold  text-white pointer-events-none opacity-1 transition-opacity duration-200 ease-in-out"} 
-                ${"top-[20px] bg-[red] right-[20px] p-[10px] rounded-[5px] rotate-[30deg]"} ${
-                translateX < -windowWidth / 10 - 10 ? "opacity-100" : "opacity-0"
-              }`}
+              className={`${"absolute z-10  text-center w-[70px] text-[1rem] font-bold  text-white pointer-events-none"} 
+                ${"top-[20px] bg-[red] right-[20px] p-[10px] rounded-[5px] rotate-[30deg]"} `}
+              style={{
+                opacity: nopeOpacity,
+              }}
             >
               NOPE
             </div>
@@ -82,7 +86,8 @@ console.log(translateX);
               className="pointer-events-none select-none  rounded-lg object-center object-cover"
             />
           </div>
-          {(index === 0 && translateX >= windowWidth / 10 + 10) ||(index === 0 && translateX <= -windowWidth / 10 - 10)||
+          {(index === 0 && translateX >= windowWidth / 10 + 10) ||
+            (index === 0 && translateX <= -windowWidth / 10 - 10) ||
             (isActive && (
               <div className="flex justify-around absolute bottom-[10px] w-full">
                 <button
@@ -98,7 +103,9 @@ console.log(translateX);
                   >
                     <path
                       d="M18.364 5.636a1 1 0 0 0-1.414 0L12 10.586 7.05 5.636a1 1 0 1 0-1.414 1.414L10.586 12l-5.95 5.95a1 1 0 1 0 1.414 1.414L12 13.414l5.95 5.95a1 1 0 1 0 1.414-1.414L13.414 12l5.95-5.95a1 1 0 0 0 0-1.414z"
-                      fill={translateX <= -windowWidth / 10 ? "#FFFFFF" : "#B40335"} // Белый крестик при активном состоянии
+                      fill={
+                        translateX <= -windowWidth / 10 ? "#FFFFFF" : "#B40335"
+                      } // Белый крестик при активном состоянии
                     />
                   </svg>
                 </button>
@@ -115,7 +122,9 @@ console.log(translateX);
                   >
                     <path
                       d="m480-121-41-37q-106-97-175-167.5t-110-126Q113-507 96.5-552T80-643q0-90 60.5-150.5T290-854q57 0 105.5 27t84.5 78q42-54 89-79.5T670-854q89 0 149.5 60.5T880-643q0 46-16.5 91T806-451.5q-41 55.5-110 126T521-158l-41 37Z"
-                      fill={translateX >= windowWidth / 10 ? "#FFFFFF" : "#36DE8D"}
+                      fill={
+                        translateX >= windowWidth / 10 ? "#FFFFFF" : "#36DE8D"
+                      }
                     />
                   </svg>
                 </button>
