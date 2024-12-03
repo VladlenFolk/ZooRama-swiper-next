32babc3adc4bed069e4d59464ecf8ef4b6ed2b6b
Avok@to34 <vladlen-17@yandex.ru>
2024-12-03 13:29:54 +0300

    feat: made customDraggables component main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import SwiperCards from "@/components/swiper-cards/SwiperCards";
+import CustomDraggables from "./HandleCardsDragging/page";
 
 export default function App() {
   return (
     <div className="w-full h-2/6">
-      <SwiperCards />
+      <CustomDraggables />
     </div>
   );
 }
