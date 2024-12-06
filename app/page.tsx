c70c23e3afa909e36128547f91c7b9ae3c01c990
Avok@to34 <vladlen-17@yandex.ru>
2024-12-06 23:53:31 +0300

    fix: tryed to improve optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import SwiperCards from "@/components/swiper-cards/SwiperCards";
 import CustomDraggables from "./HandleCardsDragging/page";
+import CustomFreeDragElement from "./HandleFreeDrag/page";
 
 export default function App() {
   return (
     <div className="w-full h-2/6">
-      <CustomDraggables />
+      <CustomFreeDragElement />
     </div>
   );
 }
