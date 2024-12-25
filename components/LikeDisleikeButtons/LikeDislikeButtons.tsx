8c5fdf3258728fb450ddab933a58f9235e01bcc0
Avok@to34 <vladlen-17@yandex.ru>
2024-12-25 15:51:58 +0300

    fix: made correct buttons  logic

--- components/LikeDisleikeButtons/LikeDislikeButtons.tsx
+++ components/LikeDisleikeButtons/LikeDislikeButtons.tsx
@@ -18,8 +18,7 @@ const LikeDislikeButtons: FC<Props> = ({
   isResetting,
 }) => {
   
-  const [rightEnter, setRightEnter] = useState(false);
-  const [leftEnter, setLeftEnter] = useState(false);
+  const [isPressed, setIsPressed] = useState<"left" | "right" | null>(null);
 
   const onLeftClick = () => {
     pressButtonDisplacement("left");
@@ -28,10 +27,10 @@ const LikeDislikeButtons: FC<Props> = ({
   const onRightClick = () => {
     pressButtonDisplacement("right");
   };
+
   useEffect(() => {
     if (isResetting) {
-      setRightEnter(false);
-      setLeftEnter(false);
+      setIsPressed(null);
     }
   }, [isResetting]);
 
@@ -47,16 +46,13 @@ const LikeDislikeButtons: FC<Props> = ({
         <div className="flex justify-around absolute bottom-[10px] w-full">
           <button
             onClick={onLeftClick}
-            onMouseLeave={() => {
-              setRightEnter(false); // Убираем активное состояние при выходе
-            }}
-            onMouseEnter={() => {
-              setRightEnter(true); // Включаем активное состояние при входе
-            }}
-            onTouchStart={() => setRightEnter(true)} // Добавлено для сенсорных устройств
-            onTouchEnd={() => setRightEnter(false)} // Добавлено для сенсорных устройств
-            className={`w-[31px] h-[31px] pointer-events-auto rounded-full border-[1px] border-[#B40335] flex items-center justify-center ${
-              translateX <= -windowWidth / 10 || rightEnter
+          onMouseDown={() => setIsPressed("left")}
+          onMouseUp={() => setIsPressed(null)}
+          onMouseLeave={() => setIsPressed(null)}
+          onTouchStart={() => setIsPressed("left")}
+          onTouchEnd={() => setIsPressed(null)}
+            className={`w-[40px] h-[40px] pointer-events-auto rounded-full border-[1px] border-[#B40335] flex items-center justify-center ${
+              translateX <= -windowWidth / 10 ||  isPressed === "left"
                 ? "bg-[#B40335]"
                 : ""
             }`}
@@ -70,7 +66,7 @@ const LikeDislikeButtons: FC<Props> = ({
               <path
                 d="M18.364 5.636a1 1 0 0 0-1.414 0L12 10.586 7.05 5.636a1 1 0 1 0-1.414 1.414L10.586 12l-5.95 5.95a1 1 0 1 0 1.414 1.414L12 13.414l5.95 5.95a1 1 0 1 0 1.414-1.414L13.414 12l5.95-5.95a1 1 0 0 0 0-1.414z"
                 fill={
-                  translateX <= -windowWidth / 10 || rightEnter
+                  translateX <= -windowWidth / 10 ||  isPressed === "left"
                     ? "#FFFFFF"
                     : "#B40335"
                 } // Белый крестик при активном состоянии
@@ -78,17 +74,14 @@ const LikeDislikeButtons: FC<Props> = ({
             </svg>
           </button>
           <button
-            onClick={onRightClick}
-            onMouseLeave={() => {
-              setLeftEnter(false); // Убираем активное состояние при выходе
-            }}
-            onMouseEnter={() => {
-              setLeftEnter(true); // Включаем активное состояние при входе
-            }}
-            onTouchStart={() => setLeftEnter(true)} // Добавлено для сенсорных устройств
-            onTouchEnd={() => setLeftEnter(false)} // Добавлено для сенсорных устройств
-            className={`w-[31px] h-[31px]  pointer-events-auto rounded-full border-[1px] border-[#36DE8D] flex items-center justify-center ${
-              translateX >= windowWidth / 10 || leftEnter ? "bg-[#36DE8D] " : ""
+             onClick={onRightClick}
+             onMouseDown={() => setIsPressed("right")}
+             onMouseUp={() => setIsPressed(null)}
+             onMouseLeave={() => setIsPressed(null)}
+             onTouchStart={() => setIsPressed("right")}
+             onTouchEnd={() => setIsPressed(null)}
+            className={`w-[40px] h-[40px]  pointer-events-auto rounded-full border-[1px] border-[#36DE8D] flex items-center justify-center ${
+              translateX >= windowWidth / 10 || isPressed === "right" ? "bg-[#36DE8D] " : ""
             }`}
           >
             <svg
@@ -100,7 +93,7 @@ const LikeDislikeButtons: FC<Props> = ({
               <path
                 d="m480-121-41-37q-106-97-175-167.5t-110-126Q113-507 96.5-552T80-643q0-90 60.5-150.5T290-854q57 0 105.5 27t84.5 78q42-54 89-79.5T670-854q89 0 149.5 60.5T880-643q0 46-16.5 91T806-451.5q-41 55.5-110 126T521-158l-41 37Z"
                 fill={
-                  translateX >= windowWidth / 10 || leftEnter
+                  translateX >= windowWidth / 10 || isPressed === "right"
                     ? "#FFFFFF"
                     : "#36DE8D"
                 }
