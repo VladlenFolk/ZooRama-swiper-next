48bd8aeb2d70a1e8fc9f0db990825c34962ddfaa
Avok@to34 <vladlen-17@yandex.ru>
2025-01-02 19:35:55 +0300

    feat: edned with dnd, hidden DnD link less then 1024px

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
     <nav className="flex gap-[40px] flex-col items-center justify-center w-full h-full">
       <Link
         href={"/HandleFreeDrag"}
-        className={`nav-link 
+        className={`nav-link flex
                   text-[#3bb8fc] border-[#3bb8fc] [text-shadow:0_0_10px_#3bb8fc] shadow-[0_0_10px_#3bb8fc] 
                     lg:hover:shadow-[0_0_20px_#3bb8fc,0_0_40px_#3bb8fc]
                     active:shadow-[0_0_20px_#3bb8fc,0_0_40px_#3bb8fc]
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
       </Link>
       <Link
         href={"/HandleCardsDragging"}
-        className={`nav-link 
+        className={`nav-link flex
                  text-[#00CC00] border-[#00CC00] [text-shadow:0_0_10px_#00CC00] shadow-[0_0_10px_#00CC00] 
                    lg:hover:shadow-[0_0_20px_#00CC00,0_0_40px_#00CC00]
                    active:shadow-[0_0_20px_#00CC00,0_0_40px_#00CC00]
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
                  text-[#FFFF00] border-[#FFFF00] [text-shadow:0_0_10px_#FFFF00] shadow-[0_0_10px_#FFFF00] 
                    lg:hover:shadow-[0_0_20px_#FFFF00,0_0_40px_#FFFF00]
                    active:shadow-[0_0_20px_#FFFF00,0_0_40px_#FFFF00]
-                   sm:hidden lg:flex
+                   hidden lg:flex
                     `}
       >
         Drag-and-Drop
