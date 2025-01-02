fd4b6799b4d9bd3a6c6291aa1004a1c10d4b0c6b
Avok@to34 <vladlen-17@yandex.ru>
2025-01-02 22:18:59 +0300

    feat: install favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import SwiperCards from "@/components/swiper-cards/SwiperCards";
-import CustomDraggables from "./HandleCardsDragging/page";
 import Home from "./Home/page";
 
 export default function App() {
